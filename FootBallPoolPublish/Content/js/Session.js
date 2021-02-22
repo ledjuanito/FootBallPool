a9641e5ab175cf25c5447493482af51511d654ec
Hiram Martinez <hiram.martinez2@outlook.com>
2021-02-22 11:38:32 -0600

    Publish Files

---
+++ FootBallPoolPublish/Content/js/Session.js
@@ -0,0 +1,23 @@
+﻿var sessionTimeout = parseInt(sessionTimeout) * 60 * 1000 + 1000;
+var sessionTimeoutWarning = sessionTimeout - 60000; //Warn 1 minute before session timeout
+
+var sessionTimeoutHandler = setTimeout('sessionEnd()', sessionTimeout);
+var sessionTimeoutWarningHandler = setTimeout('sessionWarning()', sessionTimeoutWarning);
+
+function sessionEnd() {
+    $('#logoff-form').submit();
+}
+
+function sessionWarning() {
+    $("#session-modal").modal("show");
+}
+
+function sessionContinue() {
+    $.get(window.location, function (data) {
+        window.clearTimeout(sessionTimeoutHandler);
+        window.clearTimeout(sessionTimeoutWarningHandler);
+
+        sessionTimeoutHandler = setTimeout('sessionEnd()', sessionTimeout);
+        sessionTimeoutWarningHandler = setTimeout('sessionWarning()', sessionTimeoutWarning);
+    });
+}
---
+++ FootBallPoolPublish/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FootBallPool.MvcApplication" Language="C#" %>
